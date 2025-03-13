042955be2a0110e789b7a848648021fc970f4212
Your Name <levi.strope@gmail.com>
2025-03-12 23:00:56 -0400

    refactor: Remove copyright and license headers from ob-aider.el

--- ob-aider.el
+++ ob-aider.el
@@ -1,7 +1,5 @@
 ;;; ob-aider.el --- Org Babel functions for Aider.el integration -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2024 Free Software Foundation, Inc.
-
 ;; Author: Levi Strope <levi.strope@gmail.com>
 ;; Maintainer: Levi Strope <levi.strope@gmail.com>
 ;; Keywords: tools, convenience, languages
@@ -9,21 +7,6 @@
 ;; Version: 0.1.0
 ;; Package-Version: 0.1.0
 ;; Package-Requires: ((emacs "27.1"))
-
-;; This file is not part of GNU Emacs.
-
-;; This program is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
